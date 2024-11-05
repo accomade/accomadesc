dc6f6b9e2ffe7cb04fdc0cd412c8eac52d597bc0
Dirk Porsche <dirk@accoma.de>
2024-11-05 20:59:38 +0100

    fix text styling

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -16,8 +16,8 @@
   <div
     style="
         min-height: {minHeight};
-        --font-size-text-text-editor: {textFontSize};
-        --font-size-header-text-editor: {headerFontSize};
+        --font-size-text: {textFontSize};
+        --font-size-header: {headerFontSize};
         "
     class="content"
     id="content"
@@ -27,29 +27,29 @@
 </div>
 
 <style>
-  .content {
-    width: 100%;
-    height: 100%;
-
-    text-align: left;
-    margin-left: 0.2rem;
-    margin-right: 0.2rem;
-  }
-
-  .content :global(*) {
-    font-size: var(--font-size-text-text-editor);
-  }
-
-  .content :global(h3) {
-    font-size: var(--font-size-header-text-editor);
-    text-align: left;
-  }
-
   .container {
     position: relative;
     height: 100%;
     width: 100%;
     background-color: var(--main-bg-color);
     color: var(--main-font-color);
+
+    .content {
+      width: 100%;
+      height: 100%;
+
+      text-align: left;
+      margin-left: 0.2rem;
+      margin-right: 0.2rem;
+
+      :global(*) {
+        font-size: var(--font-size-text);
+      }
+
+      :global(h3) {
+        font-size: var(--font-size-header);
+        text-align: left;
+      }
+    }
   }
 </style>
