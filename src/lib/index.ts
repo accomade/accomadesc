dd7267a7543885e091eaf6768c264106b84adfef
Dirk Porsche <dirk@accoma.de>
2024-11-10 15:10:32 +0100

    added exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,81 @@
 // Reexport your entry components here
+
+import { randomName, randomID } from './names/gen.ts';
+
+import Avatar from './basic/Avatar.svelte';
+import Button from './basic/Button.svelte';
+import Icon from './basic/Icon.svelte';
+import Spinner from './basic/Spinner.svelte';
+import TextInput from './basic/TextInput.svelte';
+
+import AccoCard from './AccoCard.svelte';
+import AccoDescription from './AccoDescription.svelte';
+import AmenitiesCore from './AmenitiesCore.svelte';
+import Calendar from './Calendar.svelte';
+import CalendarAvailable from './CalendarAvailable.svelte';
+import LeafletMap from './LeafletMap.svelte';
+import Photo from './Photo.svelte';
+import PhotoGallery from './PhotoGallery.svelte';
+import Pricing from './Pricing.svelte';
+import PricingShort from './PricingShort.svelte';
+import Section from './Section.svelte';
+import Text from './Text.svelte';
+import Weather from './Weather.svelte';
+
+export type {
+  GridPhoto,
+  LeafletMap as LeafletMapI,
+  LeafletMapContent,
+  Calendar as CalendarI,
+  CalendarContent,
+  CalendarAvailable as CalendarAvailableI,
+  CalendarAvailableContent,
+  Text as TextI,
+  TextContent,
+  Weather as WeatherI,
+  WeatherContent,
+  Photo as PhotoI,
+  PhotoContent,
+  PhotoGallery as PhotoGalleryI,
+  PhotoGalleryContent,
+  Pricing as PricingI,
+  PricingContent,
+  PricingShort as PricingShortI,
+  PricingShortContent,
+  PricingEntry,
+  PricingRange,
+  StaticRangeDate,
+  StaticPricingRange,
+  AmenitiesCore as AmenitiesCoreI,
+  AmenitiesCoreContent,
+  AccoCard as AccoCardI,
+  AccoCardContent,
+  CardContent,
+  AccoDescription as AccoDescriptionI,
+  AccoDescriptionContent,
+} from './types.ts';
+
+export { PRICING_COLUMNS } from './types.ts';
+
+export {
+  randomID,
+  randomName,
+  Avatar,
+  Button,
+  Icon,
+  Spinner,
+  TextInput,
+  AccoCard,
+  AccoDescription,
+  AmenitiesCore,
+  Calendar,
+  CalendarAvailable,
+  LeafletMap,
+  Photo,
+  PhotoGallery,
+  Pricing,
+  PricingShort,
+  Section,
+  Text,
+  Weather,
+};
