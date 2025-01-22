862d729a33cc678c1b7bd21bd0fdc16dd57249bb
Dirk Porsche <dirk@accoma.de>
2025-01-22 23:31:07 +0100

    fix css

--- src/routes/css.ts
+++ src/routes/css.ts
@@ -1,47 +1,71 @@
 export const css = `
-  --button-font-color: #070707;
-  --button-bg-color: rgb(234, 250, 255);
+  --main-font-color: #070707;
+  --main-bg-color: white;
+  --main-border-color: rgb(2, 48, 71);
+  --main-border: 1px solid var(--main-border-color);
 
-  --button-danger-font-color: #070707;
-  --button-danger-bg-color: #B01313;
+  --alert-font-color: #c50000;
+  --disabled-font-color: rgba(35,35,35, 0.4);
+  --accept-font-color: #0D9D00;
+
+  --avatar-border: 0.15rem solid rgba(7, 7, 7, 0.9);
+
+  --hamburger-color: rgb(7, 7, 7);
+  --hamburger-bg-color: transparent;
+  --hamburger-filter: drop-shadow( 0.2rem 0.2rem 0.2rem white);
 
   --gallery-button-bg-color: rgba(242, 242, 242, 0.3);
   --gallery-button-font-color: rgba(15, 14, 15, 0.6);
 
- 
+  --button-font-color: #070707;
+  --button-bg-color: rgb(234, 250, 255);
+  --button-danger-font-color: #070707;
+  --button-danger-bg-color: #B01313;
+  --button-focus-filter: drop-shadow(0 0 0.75rem rgba(102, 120, 243, 0.9));
+  --button-focussed-border-color:;
+  --button-disabled-border-color:;
+  --button-border-color:;
+
   --focussed-border-color: rgba(102, 120, 243, 0.9);
+  --focussed-border: 0.15rem solid var(--focussed-border-color); 
+  --focussed-filter: drop-shadow(0 0 0.75rem rgba(102, 120, 243, 0.9));
   --disabled-border-color: rgba(35, 35, 35, 0.5);
+  --disabled-border: 0.1rem solid var(--disabled-border-color);
+  --invalid-border-color: #B01313;
+  --invalid-border: 0.15rem solid var(--invalid-border-color);
 
-  --font-disabled-color: rgba(35,35,35, 0.4);
+  --icon-not-color: #c50000;
+  --icon-color: black;
+  --icon-disabled-color: rgba(35,35,35, 0.4);
 
-  --main-border: 1px solid rgb(2, 48, 71);
-  --notes-vert-line: 0.5px dashed rgba(2, 48, 71, .5);
-  --main-bg-color: white;
-  --main-font-color: #070707;
-  --alert-font-color: #c50000;
-  --not-color: #c50000;
-  --accept-font-color: #0D9D00;
-
-  --table-bg-color-odd: rgb(243, 243, 243);
-  --table-bg-color-even: rgb(226, 226, 226);
-  --table-font-color-odd: black;
-  --table-font-color-even: black;
+  --table-odd-bg-color: rgb(243, 243, 243);
+  --table-even-bg-color: rgb(226, 226, 226);
+  --table-odd-font-color: black;
+  --table-even-font-color: black;
   --table-header-bg-color: rgb(37, 37, 37);
   --table-header-font-color: rgb(243, 243, 243);
   --table-footer-bg-color: rgb(37, 37, 37);
   --table-footer-font-color: rgb(243, 243, 243);
   
-  --attribution-bg-color: rgba(225, 225, 225, 0.6);
-  --attribution-font-color: #070707;
-  
   --picture-link-bg-color: rgba(51, 51, 51, 0.8);
   --picture-link-font-color: rgb(168, 194, 250);
   --external-link-font-color: rgb(105, 152, 252);
+  --attribution-bg-color: rgba(225, 225, 225, 0.6);
+  --attribution-font-color: #070707;
+  --picture-frame-shadow: 
+      0px 1.1px 2.2px rgba(0, 0, 0, 0.02),
+      0px 2.7px 5.3px rgba(0, 0, 0, 0.028),
+      0px 5px 10px rgba(0, 0, 0, 0.035),
+      0px 8.9px 17.9px rgba(0, 0, 0, 0.042),
+      0px 16.7px 33.4px rgba(0, 0, 0, 0.05),
+      0px 40px 80px rgba(0, 0, 0, 0.07);
+  
  
   --nav-bg-color: rgba(7,7,7, 0.8);
   --nav-font-color: white; 
 
   --title-font-color: white;
+  --title-filter: drop-shadow(0.2rem 0.3rem 0.2rem black);
   
   --footer-font-color: white;
   --footer-bg-color: #070707;
@@ -58,23 +82,23 @@ export const css = `
       8.9px 28.2px 30.7px rgba(0, 0, 0, 0.046),
       13.7px 43.1px 43.8px rgba(0, 0, 0, 0.053),
       19px 60px 80px rgba(0, 0, 0, 0.07);
-  --acco-card-font: black;
-  --acco-card-bg: white;
+  --acco-card-font-color: black;
+  --acco-card-bg-color: white;
 
   --occuplan-main-border: 1px solid rgb(2, 48, 71);
   --occuplan-grid-border: 0.2px solid rgba(2, 48, 71, 0.2);
-  --occuplan-font-color-main: rgb(2, 48, 71);
-  --occuplan-font-color-days: rgb(2, 48, 71);
-  --occuplan-font-color-days-header: rgb(2, 48, 71);
-  --occuplan-font-color-weeknum: rgba(2, 48, 71, 0.5);
-  --occuplan-font-color-months: rgb(2, 48, 71);
-  --occuplan-font-color-invalid-days: rgb(25, 25, 25);
-  --occuplan-bg-color-days-header: rgb(142, 202, 230);
-  --occuplan-bg-color-weeknum: transparent;
-  --occuplan-bg-color-months: rgb(142, 202, 230);
-  --occuplan-bg-color-main: transparent;
-  --occuplan-bg-color-invalid-days: rgba(110,110,110,0.6);
-  --occuplan-bg-color-weekend: rgb(2, 48, 71);
+  --occuplan-main-font-color: rgb(2, 48, 71);
+  --occuplan-days-font-color: rgb(2, 48, 71);
+  --occuplan-days-header-font-color: rgb(2, 48, 71);
+  --occuplan-weeknum-font-color: rgba(2, 48, 71, 0.5);
+  --occuplan-month-font-color: rgb(2, 48, 71);
+  --occuplan-invalid-days-font-color: rgb(25, 25, 25);
+  --occuplan-days-header-bg-color: rgb(142, 202, 230);
+  --occuplan-weeknum-bg-color: transparent;
+  --occuplan-months-bg-color: rgb(142, 202, 230);
+  --occuplan-main-bg-color: transparent;
+  --occuplan-invalid-days-bg-color: rgba(110,110,110,0.6);
+  --occuplan-weekend-bg-color: rgb(2, 48, 71);
 
   --occupation-type-1-font-color: rgb(2, 48, 71);
   --occupation-type-1-bg-color: rgb(33, 158, 188);
@@ -84,7 +108,9 @@ export const css = `
   --occupation-type-3-bg-color: #007017;
 
 
-  --longinput-bg-color: #fff7dd;
-  --longinput-disabled-bg-color: #eeefde;
-  --longinput-font-color: #160067;
+  --notes-vert-line: 0.5px dashed rgba(2, 48, 71, .5);
+  --notes-font-color: #160067;
+  --notes-bg-color: #fff7dd;
+  --notes-disabled-font-color: #160067;
+  --notes-disabled-bg-color: #eeefde;
 `;
