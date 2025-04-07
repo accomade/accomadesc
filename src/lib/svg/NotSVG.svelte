9db33235fc5d820ed7300d824d19bc29f24959dc
Dirk Porsche <dirk@accoma.de>
2025-04-07 17:23:31 +0200

    fix: colors

--- src/lib/svg/NotSVG.svelte
+++ src/lib/svg/NotSVG.svelte
@@ -17,13 +17,13 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="30" cy="30" r="26" stroke="var(--icon-not-color, 'red')" stroke-width="8" />
+    <circle cx="30" cy="30" r="26" stroke="var(--icon-not-color, red)" stroke-width="8" />
     <line
       x1="12.8669"
       y1="13.2106"
       x2="48.8669"
       y2="50.2106"
-      stroke="var(--icon-not-color, 'red')"
+      stroke="var(--icon-not-color, red)"
       stroke-width="8"
     />
   </svg>
