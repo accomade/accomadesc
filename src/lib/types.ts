e40b280ec4552123e26918d99e9961ce550bb4c6
Crenshinibon <dirk@accoma.de>
2026-01-19 22:35:29 +0100

    isTos boolean

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -452,7 +452,7 @@ export interface PageProps {
   header?: string;
   showFooter?: boolean;
   footerRef?: string;
-  isToS?: string;
+  isToS?: boolean;
   fixedHamburger?: boolean;
   navbarOverHamburger?: boolean;
   disableLinks?: boolean;
